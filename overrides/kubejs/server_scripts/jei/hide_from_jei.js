29948405a3f284c8ed1faa3eab13fd8881cd360d
ishinyca <ishinyca@gmail.com>
2026-05-08 00:07:37 -0400

    added a comment to hide jei.js

--- overrides/kubejs/server_scripts/jei/hide_from_jei.js
+++ overrides/kubejs/server_scripts/jei/hide_from_jei.js
@@ -1,6 +1,7 @@
 // Reference: https://kubejs.com/wiki/events/RecipeViewerEvents
 
 const hide_jei_items = [
+    // Mekanism
     'mekanism:mekasuit_helmet',
     'mekanism:mekasuit_bodyarmor',
     'mekanism:mekasuit_pants',
