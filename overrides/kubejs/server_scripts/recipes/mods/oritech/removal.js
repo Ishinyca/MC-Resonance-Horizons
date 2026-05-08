5c2630b1b2e8a4aff035fe9e9f4c295c1f4088eb
ishinyca <ishinyca@gmail.com>
2026-05-08 00:08:47 -0400

    removed duplicate recipes and compats for oritech

---
+++ overrides/kubejs/server_scripts/recipes/mods/oritech/removal.js
@@ -0,0 +1,14 @@
+const duplicateOritechItems = [
+    'oritech:compat/enderio/crafting/fluidpipe',
+    'oritech:mixing/compat/create/duratium',
+    'oritech:compat/enderio/alloy/duratium',
+    'oritech:compat/immersiveengineering/alloying/duratium',
+    'oritech:compat/immersiveengineering/arcalloying/duration',
+    'oritech:compat/productivemetalworks/casting/ingot/duratium',
+];
+
+ServerEvents.recipes(event => {
+    duplicateOritechItems.forEach(recipeId =>{
+        event.remove({id:recipeId});
+    });
+});
