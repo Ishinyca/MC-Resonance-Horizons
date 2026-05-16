b9bc9990351ba14f017e894134b09853946ddb37
ishinyca <ishinyca@gmail.com>
2026-05-16 15:07:14 -0400

    Removed invar alloying for now

---
+++ overrides/kubejs/server_scripts/recipes/mods/productive_metalworks/invar.js
@@ -0,0 +1,6 @@
+// Productive Metalworks
+// -- Invar is currently not used in any recipes so will remove it for the time being
+
+ServerEvents.recipes(event => {
+    event.remove({id: 'productivemetalworks:alloying/molten_invar'})
+})
