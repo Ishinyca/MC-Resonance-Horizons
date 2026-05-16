b2b4f9be8ba63a05def18213d0f8a7f6948b903f
ishinyca <ishinyca@gmail.com>
2026-05-16 01:25:37 -0400

    Tag updates

--- overrides/kubejs/server_scripts/tags/items.js
+++ overrides/kubejs/server_scripts/tags/items.js
@@ -2,9 +2,22 @@ const pamHarvestStockIngredientRemoval = [
     'minecraft:bone',
 ]
 
+const sawdustTagsRemoval = [
+    'c:dusts',
+    'c:dusts/wood',
+    'supplementaries:hourglass_dusts',
+]
+
 ServerEvents.tags('item', event => {
     pamHarvestStockIngredientRemoval.forEach( pamRemoval => {
         event.remove('c:stock_ingredients', pamRemoval)
         event.remove('c:stock_ingredients/bone', pamRemoval)
     })
+
+    sawdustTagsRemoval.forEach( sawdustRemoval => {
+        event.remove(sawdustRemoval, 'createdieselgenerators:wood_chip')
+    })
+    // event.remove('c:dusts', 'createdieselgenerators:wood_chip')
+    // event.remove('c:dusts/wood', 'createdieselgenerators:wood_chip')
+    // event.remove('supplementaries:hourglass_dusts', 'createdieselgenerators:wood_chip')
 })
